49a66a35f9ca64d0d15f674cbfeb8902338a78b5
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 22:13:54 +0500

    Добавил комментарий про пагинацию

--- project/project/settings_rest_framework.py
+++ project/project/settings_rest_framework.py
@@ -5,6 +5,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
+    
+    # Настраиваю пагинацию
     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
         'PAGE_SIZE' : 10
 }
