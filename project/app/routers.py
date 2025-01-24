b0b81b9af5599b6e131c21fcdbd9fa4b2923168e
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 21:27:15 +0500

    Добавил новый роут для group

--- project/app/routers.py
+++ project/app/routers.py
@@ -1,6 +1,10 @@
 from rest_framework import routers
-from .views import UserViewSet
+from .views import (
+    UserViewSet,
+    GroupViewSet,
+    )
 
 # Routers provide an easy way  of automatically determining the URL conf
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'group', GroupViewSet)
