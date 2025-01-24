211d133c8b5e4a8da60d169032451360d67bf653
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 23:39:44 +0500

    Сформировал модель задания

--- project/app/models.py
+++ project/app/models.py
@@ -6,7 +6,17 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     autor = models.ForeignKey(User, on_delete=models.PROTECT)
     create_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    shot_task_deskriptions = models.CharField(max_length=30, verbose_name="Описвание задачи")
     task_descriptions = models.TextField(verbose_name="Задача")
     due_time = models.DateTimeField(verbose_name="Исполнить до")
     сompleted_date = models.DurationField(verbose_name="Дата выполнения")
     is_done = models.BooleanField(verbose_name="Исполнено")
+
+    def __str__(self):
+        return(
+            f"{self.autor} | {self.create_at} | {self.shot_task_deskriptions} "
+        )
+    
+    class Meta:
+        verbose_name = "Задание"
+        vorbose_name_plural = "Задания"
