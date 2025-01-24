5a8624344c17c8bc04707792872e045f5a612f0f
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 16:21:01 +0500

    Вынес вид в соответствующий файл

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+
+from app.serializers import UserSerializer
 
 # Create your views here.
+
+
+# ViewSets define the user behavoir
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
