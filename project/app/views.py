77ada12e885d27e46d5d7ce29d9a97115aad96bf
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 21:24:18 +0500

    Добавил представления для вивода group, добавил проврку(???) авторизации

--- project/app/views.py
+++ project/app/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import (
     User,
     Group,
     )
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    permissions,
+    )
 
 from app.serializers import (
     UserSerializer,
@@ -15,5 +18,17 @@ from app.serializers import (
 
 # ViewSets define the user behavoir
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    """
+        API endpoint that allows users to be viewed or edited
+    """ 
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+        API endpoin that allows groups to be viewed or edited
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
