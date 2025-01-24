134f724bafe4ba98899bc13f514a23643fc01d7f
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 23:58:38 +0500

    Поправил текстовую ошибку а поле короткого опичания и бодавил возможность пустого поля в "Дата выполнения"

--- project/app/models.py
+++ project/app/models.py
@@ -6,10 +6,10 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     autor = models.ForeignKey(User, on_delete=models.PROTECT)
     create_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    shot_task_deskriptions = models.CharField(max_length=30, verbose_name="Описвание задачи")
+    shot_task_deskriptions = models.CharField(max_length=30, verbose_name="Описание задачи")
     task_descriptions = models.TextField(verbose_name="Задача")
     due_time = models.DateTimeField(verbose_name="Исполнить до")
-    сompleted_date = models.DurationField(verbose_name="Дата выполнения")
+    сompleted_date = models.DurationField(blank=True, null=True, verbose_name="Дата выполнения")
     is_done = models.BooleanField(verbose_name="Исполнено")
 
     def __str__(self):
@@ -19,4 +19,4 @@ class Task(models.Model):
     
     class Meta:
         verbose_name = "Задание"
-        vorbose_name_plural = "Задания"
+        verbose_name_plural = "Задания"
