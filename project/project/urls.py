2403d163a76f918859347b58e6846b8b9b8e99b4
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 15:44:27 +0500

    Попробовал декомпозицию - не понял, получилось или нет

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+
+from django.urls import(
+    path,
+    include
+)
+from django.contrib.auth.models import User
+from rest_framework import (
+    routers,
+    serializers,
+    viewsets
+)
+
+from .urls_api import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Wire up our API using automatic URL routing
+    # Additionally, we include login URLs for the brousable API
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
