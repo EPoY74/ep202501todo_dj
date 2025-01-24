268d320144893e55827e64c2f5a48d87ad249340
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 23:22:42 +0500

    Добавляю модель задачи

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    autor = models.ForeignKey(User, on_delete=models.PROTECT)
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    task_descriptions = models.TextField(verbose_name="Задача")
+    due_time = models.DateTimeField(verbose_name="Исполнить до")
+    сompleted_date = models.DurationField(verbose_name="Дата выполнения")
+    is_done = models.BooleanField(verbose_name="Исполнено")
