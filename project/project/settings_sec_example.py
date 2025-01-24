8eaee596e4819ad1146c4bb1fb6073d127b5fd87
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 23:21:56 +0500

    Поправил файл с примером секретных настроек

--- project/project/settings_sec_example.py
+++ project/project/settings_sec_example.py
@@ -1,17 +1,22 @@
-from pathlib import Path  #
-
+from pathlib import Path  ##
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '************************'
+SECRET_KEY = 'Это твой секретный ключ'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# docker run --name todo-postgres -p 5432:5432 -e POSTGRES_PASSWORD=PassWord -d postgres
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+DB_ENGINE =  'django.db.backends.sqlite3'
+DB_NAME = BASE_DIR / 'db.sqlite3'
+
+DB_ENGINE_PG = 'django.db.backends.postgresql'
+DB_NAME_PG = 'Имя базы данных'
+DB_USER_PG = 'Пользователь БД'
+DB_PASSWORD_PG = 'Пароль БД'
+DB_HOST_PG = 'Хост БД'
+DB_PORT_PG = 'Порт в БД'
+
