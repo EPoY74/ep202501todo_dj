aa915441f2c697932a8d37e3cc998397a30461f6
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-24 20:53:17 +0500

    Добавил Сериалайзер для Group

--- project/app/serializers.py
+++ project/app/serializers.py
@@ -1,4 +1,7 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import (
+    User,
+    Group,
+    )
 from rest_framework import serializers
     
 
@@ -7,3 +10,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=User
         fields = ['url', 'username', 'email', 'is_staff']
+
+class GroupSerialization(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=Group
+        feelds = ['url', 'name']
