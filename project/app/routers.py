5d62ab3cc5ca3a4788e9a548edc401829dd417a2
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-28 20:06:09 +0500

    Добавил роутер для вывода task

--- project/app/routers.py
+++ project/app/routers.py
@@ -2,9 +2,11 @@ from rest_framework import routers
 from .views import (
     UserViewSet,
     GroupViewSet,
+    TaskViewSet
     )
 
 # Routers provide an easy way  of automatically determining the URL conf
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'group', GroupViewSet)
+router.register(r'task',TaskViewSet)
